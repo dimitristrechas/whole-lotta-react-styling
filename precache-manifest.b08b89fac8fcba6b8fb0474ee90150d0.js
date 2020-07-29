8e1912f7e08d296f8123604e1c58710da62413f8
dimitristrechas <dimitristrechas@gmail.com>
2020-07-29 22:49:56 +0300

    Updates

---
+++ precache-manifest.b08b89fac8fcba6b8fb0474ee90150d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eab94ebce3aa69bdca80d76046600eca",
+    "url": "/dimitristrechas/whole-lotta-react-styling/index.html"
+  },
+  {
+    "revision": "4df84f88625f7fe8d696",
+    "url": "/dimitristrechas/whole-lotta-react-styling/static/css/main.555443a3.chunk.css"
+  },
+  {
+    "revision": "b5c12813f9fd9bc9b0ed",
+    "url": "/dimitristrechas/whole-lotta-react-styling/static/js/2.098d9ca9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/dimitristrechas/whole-lotta-react-styling/static/js/2.098d9ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df84f88625f7fe8d696",
+    "url": "/dimitristrechas/whole-lotta-react-styling/static/js/main.5c8fead9.chunk.js"
+  },
+  {
+    "revision": "faa94cf9e45d806f2463",
+    "url": "/dimitristrechas/whole-lotta-react-styling/static/js/runtime-main.bcdf95c1.js"
+  }
+]);
