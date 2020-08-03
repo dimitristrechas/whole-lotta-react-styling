ff85b16ef8c3adc7957d64aaaf99cfb137a4a5af
dimitristrechas <dtrechas@singularlogic.eu>
2020-08-03 15:37:25 +0300

    added footer header

--- src/App.js
+++ src/App.js
@@ -9,15 +9,27 @@ import "github-markdown-css";
 
 export default function App() {
   return (
-    <div className="main">
-      <h1>Whole Lotta React Styling</h1>
-      <hr></hr>
-      <TestCss />
-      <TestSass />
-      <TestCssModule />
-      <TestSassModule />
-      <TestEmotion />
-      <TestJss />
+    <div className="container">
+      <header>
+        <h1>Whole Lotta React Styling</h1>
+      </header>
+      <main>
+        <TestCss />
+        <TestSass />
+        <TestCssModule />
+        <TestSassModule />
+        <TestEmotion />
+        <TestJss />
+      </main>
+      <footer>
+        <a
+          href="https://dimitristrechas.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          dimitristrechas.com
+        </a>
+      </footer>
     </div>
   );
 }
